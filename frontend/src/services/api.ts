cbb5d658f2565ef32cfecda7257a54e505a99f84
jeid12 <niyokwizerajd123@gmail.com>
2025-06-17 18:36:01 +0200

    update on frontend

--- frontend/src/services/api.ts
+++ frontend/src/services/api.ts
@@ -1,7 +1,7 @@
 import axios from 'axios';
 import categoryAPI from './api/categoryAPI';
 
-const API_URL = import.meta.env.VITE_API_URL || 'http://localhost:5000/api';
+const API_URL = 'https://citizen-engagement-system-3.onrender.com/api';
 
 const api = axios.create({
     baseURL: API_URL,
